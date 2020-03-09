752658e598013336f892b420a60f2d6dbcf9c534
rssfrncs <rssfrncs@gmail.com>
2020-03-09 22:20:48 +0000

    Updates

---
+++ precache-manifest.85a38c19265a07408217fc990dcf90eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7cc11b2e579a34dcf443879846b70c",
+    "url": "/ledger/index.html"
+  },
+  {
+    "revision": "4693b349a39091eb2a03",
+    "url": "/ledger/static/css/main.2af78c06.chunk.css"
+  },
+  {
+    "revision": "47eeac32dd402d5bdbf3",
+    "url": "/ledger/static/js/2.0a088f30.chunk.js"
+  },
+  {
+    "revision": "54ee72905bd1a3a124ada633930d4066",
+    "url": "/ledger/static/js/2.0a088f30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4693b349a39091eb2a03",
+    "url": "/ledger/static/js/main.cfe172bd.chunk.js"
+  },
+  {
+    "revision": "e380e111cf2e2bb58810",
+    "url": "/ledger/static/js/runtime-main.aaed06a0.js"
+  }
+]);
