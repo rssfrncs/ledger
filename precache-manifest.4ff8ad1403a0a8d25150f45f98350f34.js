9e39f2d7684a65fc36e58ba13d3552f0be92cf73
ross francis <rssfrncs"gmail.com>
2020-03-10 07:19:21 +0000

    Updates

---
+++ precache-manifest.4ff8ad1403a0a8d25150f45f98350f34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c59d26c20716284c769c2b05f4413d00",
+    "url": "/ledger/index.html"
+  },
+  {
+    "revision": "4da56ee7b191f1af4504",
+    "url": "/ledger/static/css/main.c67d61b6.chunk.css"
+  },
+  {
+    "revision": "0d4d182b8b20fcb27d5b",
+    "url": "/ledger/static/js/2.9e5dacd3.chunk.js"
+  },
+  {
+    "revision": "54ee72905bd1a3a124ada633930d4066",
+    "url": "/ledger/static/js/2.9e5dacd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4da56ee7b191f1af4504",
+    "url": "/ledger/static/js/main.5613cf4b.chunk.js"
+  },
+  {
+    "revision": "e380e111cf2e2bb58810",
+    "url": "/ledger/static/js/runtime-main.aaed06a0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ledger/precache-manifest.d20d02025f12144964318b6def0cbbe7.js"
+  "/ledger/precache-manifest.4ff8ad1403a0a8d25150f45f98350f34.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ledger/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
